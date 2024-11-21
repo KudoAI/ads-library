fb47a39289917cc9d1dce7bb7c51e8700a646257
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-21 14:41:25 -0800

    Removed unneeded JavaScript blocks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,23 +4,6 @@ import markdown from '@eslint/markdown'
 import eslintPluginYml from 'eslint-plugin-yml'
 
 export default [
-    {
-        files: ['**/*.js', '**/*.mjs'],
-        languageOptions: { ecmaVersion: 'latest', sourceType: 'script' },
-        rules: {
-            ...js.configs.recommended.rules,
-            'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
-            'quotes': ['error', 'single'], // enforce single quotes for string literals
-            'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
-            'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
-            'no-constant-condition': 'off', // allow constant conditions
-            'no-empty': 'off', // allow empty blocks
-            'no-inner-declarations': 'off', // allow function declarations anywhere
-            'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
-            'no-unused-vars': ['error', { 'caughtErrors': 'none' }] // allow unused named args in catch blocks
-        }
-    },
-    { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' }},
     { files: ['**/*.json'], ignores: ['**/package-lock.json'], language: 'json/json', ...json.configs.recommended },
     {
         files: ['**/*.md'], language: 'markdown/commonmark', plugins: { markdown },
